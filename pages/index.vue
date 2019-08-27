2c5524cb166ddb4e14a61979f9bbb64787f9c16a
Andre Monello <atmonello@gmail.com>
2019-08-27 20:20:56 -0300

    Variáveis de ambiente
    - Adicionadas variáveis env no arquivo de configuração do Nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,22 @@
       <v-col cols="6">
         <h1>Intelipost</h1>
       </v-col>
+      <v-col cols="6">
+        <p>{{ api }}</p>
+      </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  asyncData(ctx) {
+    return {
+      api: {
+        youtubeKey: ctx.env.youtubeApiKey,
+        ticketmasterKey: ctx.env.ticketmasterApiKey
+      }
+    };
+  }
+};
+</script>
